0faf9baf9f0a54289fa98a767d4631cb2a735567
Sina Solaimanpour <sina@uga.edu>
2015-01-08 12:11:45 -0500

    Adding initial project files

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Sina Solaimanpour
+// Version     :
+// Copyright   : 
+// Description : main file for the project
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "ColonD Image Processor" << endl; // prints ColonD Image Processor
+	return 0;
+}
